9381ba46b6b17abe1e6d44abf5ab6d99e13991aa
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-13 21:54:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # dog.rb
+class Dog 
+  attr_accessor :bark, :sit
+  
+def initialize(bark,sit)
+  @bark = bark
+  @sit = sit 
+  end 
+end 
+
+dog_action = Dog.new("Woof!","The Dog is sitting.")
+
+puts dog_action.bark
+puts dog_action.sit 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 # person.rb
+class Person 
+  attr_accessor :talk, :walk
+  
+def initialize(talk,walk)
+  @talk = talk
+  @walk = walk
+  end 
+end 
+
+person_action = Person.new("Hello World!","The Person is walking.")
+
+puts person_action.talk
+puts person_action.walk
